3a0be779982262b1d76038d3d5df684901a5c934
olaElobeid <ola.elobeid81@gmail.com>
2025-07-08 23:07:57 +0200

    Scaffolding and migration

---
+++ MovieApi/Data/MovieApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Models.Entities;
+
+namespace MovieApi.Data
+{
+    public class MovieApiContext : DbContext
+    {
+        public MovieApiContext (DbContextOptions<MovieApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieApi.Models.Entities.Movie> Movie { get; set; } = default!;
+    }
+}
